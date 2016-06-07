ca51c81611a99a67c3339a2ef6ba9784c53a1dc6
Vladislav Kuspits <vkuspits@mirantis.com>
2016-06-07 15:21:52 +0300

    Puppet scripts for nfs added

--- deployment_scripts/deploy-nfs-client-override.pp
+++ deployment_scripts/deploy-nfs-client-override.pp
@@ -1,11 +1,8 @@
-notice('MODULAR: nfs-service.pp')
+notice('MODULAR: nfs-client.pp')
 
 
 $nfs_plugin    = hiera('nfs-service', {})
 $metadata      = pick($nfs_plugin['metadata'], {})
-$data_folder   = $metadata['nfs-share-dir']
-$network       = hiera('network_metadata', {})
-$storage_net   = $network['storage']
 
 $hiera_dir     = '/etc/hiera/override'
 $plugin_yaml   = 'nfs-service.yaml'
@@ -37,17 +34,14 @@ corosync_roles:
     line  => "  - override/${plugin_name}",
     after => '  - override/module/%{calling_module}',
   }
-  
+
   module {'haraldsk-nfs':
     ensure => present,
   }
   
-  node server {
-    include nfs::server
-    nfs::server::export{ '/$data_folder':
-       ensure  => 'mounted',
-       clients => '${storage_net}(rw,insecure,async,no_root_squash) localhost(rw)'
-    }
-    require => Module['haraldsk-nfs'],
-  }  
+  node client {
+   include nfs::client
+   Nfs::Client::Mount <<| |>>
+   require => Module['haraldsk-nfs'], 
+  }
 }
